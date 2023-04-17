847827c5fba7327fee97742d0b04c413cf94539f
Saied <elsaied.samaka1997@gmail.com>
2023-04-17 12:56:08 +0200

    wrap up delete all functionality

--- src/app/employees/components/employee-form-delete/employee-form-delete.component.ts
+++ src/app/employees/components/employee-form-delete/employee-form-delete.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-employee-form-delete',
@@ -9,7 +9,7 @@ export class EmployeeFormDeleteComponent implements OnInit {
   showConfiramtionModel = false;
   displayConfirmationModel = 'none';
   empIds;
-
+  @Input() isEmplyessChecked: boolean;
   constructor() {}
 
   ngOnInit() {}
