d2e0a9e915ccbe07070999c0770e4f5899aa80ae
Saied <elsaied.samaka1997@gmail.com>
2023-05-06 14:11:06 +0300

    wrap up

--- src/app/employees/shared/pagination/pagination.component.ts
+++ src/app/employees/shared/pagination/pagination.component.ts
@@ -7,7 +7,7 @@ import { EmpolyeesService } from '../../services/employees.service';
   styleUrls: ['./pagination.component.css'],
 })
 export class PaginationComponent implements OnInit {
-  @Input() currentPage: number = 1;
+  @Input() currentPage: number = 0;
   totalPages: number = 0;
   totalItems: number = 0;
   @Output() goTo: EventEmitter<number> = new EventEmitter<number>();
@@ -19,9 +19,7 @@ export class PaginationComponent implements OnInit {
     this.empSer.getEmployees$.subscribe((res) => {
       this.totalPages = Math.ceil(res.length / 10);
       this.totalItems = res.length;
-      this.pages = Array.from(Array(this.totalPages).keys()).filter(
-        (pageNumber) => pageNumber >= 1 && pageNumber <= this.totalPages
-      );
+      this.pages = this.getPages(this.currentPage, this.totalPages);
     });
   }
 
@@ -39,5 +37,24 @@ export class PaginationComponent implements OnInit {
   public onPrevious(): void {
     console.log('Previous page');
     this.previous.emit(this.currentPage);
+  }
+  getPages(current: number, total: number): number[] {
+    if (total <= 7) {
+      console.log('total <= 7');
+      return Array.from(Array(total).keys()).map((x) => ++x);
+    }
+
+    if (current > 5) {
+      console.log('current > 5');
+      if (current >= total - 4) {
+        console.log('current >= total - 4');
+        return [1, -1, total - 4, total - 3, total - 2, total - 1, total];
+      } else {
+        console.log('current < total - 4');
+        return [1, -1, current - 1, current, current + 1, -1, total];
+      }
+    }
+    console.log('current < 5');
+    return [1, 2, 3, 4, 5, -1, total];
   }
 }
